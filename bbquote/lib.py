d063333c19dfb9f34ff3aa4c359afd37d8f632d5
Eng Yong <jack950717@hotmail.com>
2022-05-31 17:26:12 +0800

    bbquote

--- bbquote/lib.py
+++ bbquote/lib.py
@@ -3,15 +3,12 @@ import requests
 
 
 def get_quote():
-    # url = 'https://breaking-bad.lewagon.com/v1/quotes'  # alternative API
-    url = 'https://wagon-breaking-bad-quotes.herokuapp.com/v1/quotes'
-    response = requests.get(url).json()[0]
+    url = 'https://breaking-bad.lewagon.com/v1/quotes'  # alternative API
+    # url = 'https://wagon-breaking-bad-quotes.herokuapp.com/v1/quotes'
+    response = requests.get(url).json()
 
     return f"'{response['quote']}' \n> {response['author']}"
 
 
 if __name__ == "__main__":
     print(get_quote())
-
-
-get_quote()
